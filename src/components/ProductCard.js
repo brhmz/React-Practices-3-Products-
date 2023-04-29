229f3eddd82385e6d39894f70f534bbf412bba1f
Ibrahim OZ <60812594+brhmz@users.noreply.github.com>
2023-04-29 23:44:12 +0200

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ function ProductCard({product}) {
 
   return (
     <div className='product-card'>
-        <img className='product-image' src = {product.image}/>
+        <img className='product-image' src = {product.image} alt={product.title}/>
         <p>{product.title}</p>
         <p>$ {product.price}</p>
         <a href={`/productdetails/${product.id}`}><button>See Details</button></a>
